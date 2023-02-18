7be70bb1bbd1f8bdfedfb82957ff29523b159753
sunil parmar <sunil04ce@gmail.com>
2023-02-18 18:50:31 +0530

    initial code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Button from "./components/UI/Button/Button";
+import './App.css';
+
+function App() {
+
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App is Running");
+
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevState) => !prevState);
+  }
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
